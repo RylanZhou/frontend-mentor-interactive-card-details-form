1a26d40f33708d91ae8a267ebe09f727c6757cdf
Rylan Zhou <rylanzhou@outlook.com>
2022-11-03 01:08:36 -0500

    core: add rule to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,9 +13,10 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: ['warn', 'single'],
     semi: ['error', 'always'],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
   },
 };
